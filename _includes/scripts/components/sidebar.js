3357a82406096b4eb92306edbe4e165108525e79
LeHaine <colt.daily@gmail.com>
2022-02-06 20:09:43 -0500

    update sidebar to scroll active element into view

--- _includes/scripts/components/sidebar.js
+++ _includes/scripts/components/sidebar.js
@@ -1,30 +1,38 @@
-(function() {
-  var SOURCES = window.TEXT_VARIABLES.sources;
+(function () {
+    var SOURCES = window.TEXT_VARIABLES.sources;
 
-  window.Lazyload.js(SOURCES.jquery, function() {
-    var $pageMask = $('.js-page-mask');
-    var $pageRoot = $('.js-page-root');
-    var $sidebarShow = $('.js-sidebar-show');
-    var $sidebarHide = $('.js-sidebar-hide');
+    window.Lazyload.js(SOURCES.jquery, function () {
+        var $pageMask = $(".js-page-mask");
+        var $pageRoot = $(".js-page-root");
+        var $sidebarShow = $(".js-sidebar-show");
+        var $sidebarHide = $(".js-sidebar-hide");
+        document.querySelector(".scroll-to").scrollIntoView({
+            behavior: "smooth",
+        });
+        function freeze(e) {
+            if (e.target === $pageMask[0]) {
+                e.preventDefault();
+            }
+        }
+        function stopBodyScrolling(bool) {
+            if (bool === true) {
+                window.addEventListener("touchmove", freeze, {
+                    passive: false,
+                });
+            } else {
+                window.removeEventListener("touchmove", freeze, {
+                    passive: false,
+                });
+            }
+        }
 
-    function freeze(e) {
-      if (e.target === $pageMask[0]) {
-        e.preventDefault();
-      }
-    }
-    function stopBodyScrolling(bool) {
-      if (bool === true) {
-        window.addEventListener('touchmove', freeze, { passive: false });
-      } else {
-        window.removeEventListener('touchmove', freeze, { passive: false });
-      }
-    }
-
-    $sidebarShow.on('click', function() {
-      stopBodyScrolling(true); $pageRoot.addClass('show-sidebar');
+        $sidebarShow.on("click", function () {
+            stopBodyScrolling(true);
+            $pageRoot.addClass("show-sidebar");
+        });
+        $sidebarHide.on("click", function () {
+            stopBodyScrolling(false);
+            $pageRoot.removeClass("show-sidebar");
+        });
     });
-    $sidebarHide.on('click', function() {
-      stopBodyScrolling(false); $pageRoot.removeClass('show-sidebar');
-    });
-  });
 })();
